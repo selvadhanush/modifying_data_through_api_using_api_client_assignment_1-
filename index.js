390f03c0047e0983ce8234f0999cf2b15bb63151
selvadhanush <selva.dhanush.s81@kalvium.community>
2025-02-26 11:43:49 +0530

    done

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const { resolve } = require('path');
 const mongoose =require('mongoose');
 require('dotenv').config();
 const MenuItem = require('./schema.js');
@@ -36,6 +35,37 @@ app.post('/menu', async (req, res) => {
       const newItem = new MenuItem({ name, description, price });
       await newItem.save();
       res.status(201).json({ message: "Menu item added successfully", data: newItem });
+  } catch (err) {
+      res.status(500).json({ error: err.message });
+  }
+});
+
+app.put('/menu/:id', async (req, res) => {
+  try {
+      const {id}=req.params;
+      const{name,description,price}=req.body;
+
+      const updatedItem=await MenuItem.findByIdAndUpdate(id,{name,description,price},{new:true,runValidators:true});
+      if(!updatedItem){
+        return res.status(404).json({ error: "Menu item not found" });
+      }
+      res.status(200).json({error:"Menu item updated successfully",data:updatedItem});
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
+
+app.delete('/menu/:id', async (req, res) => {
+  try {
+      const { id } = req.params;
+
+      const deletedItem = await MenuItem.findByIdAndDelete(id);
+
+      if (!deletedItem) {
+          return res.status(404).json({ error: "Menu item not found" });
+      }
+
+      res.status(200).json({ message: "Menu item deleted successfully" });
   } catch (err) {
       res.status(500).json({ error: err.message });
   }
