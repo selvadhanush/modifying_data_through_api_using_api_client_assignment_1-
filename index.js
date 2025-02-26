0728482c5e1a28e6073e777623351a38c7705585
selvadhanush <selva.dhanush.s81@kalvium.community>
2025-02-26 10:49:42 +0530

    done

--- index.js
+++ index.js
@@ -1,13 +1,44 @@
 const express = require('express');
 const { resolve } = require('path');
+const mongoose =require('mongoose');
+require('dotenv').config();
+const MenuItem = require('./schema.js');
 
 const app = express();
-const port = 3010;
+app.use(express.json());
+
+const port=process.env.port
+
+mongoose.connect(process.env.mongo_url)
+.then(()=>
+  console.log("MongoDB Connected")
+).catch(err=>
+  console.log(err)
+)
 
 app.use(express.static('static'));
 
-app.get('/', (req, res) => {
-  res.sendFile(resolve(__dirname, 'pages/index.html'));
+app.get('/menu', async (req, res) => {
+  try {
+      const menuItems = await MenuItem.find();
+      res.status(200).json(menuItems);
+  } catch (err) {
+      res.status(500).json({ error: err.message });
+  }
+});
+
+app.post('/menu', async (req, res) => {
+  try {
+      const { name, description, price } = req.body;
+      if (!name || price == null) {
+          return res.status(400).json({ error: "Name and price are required." });
+      }
+      const newItem = new MenuItem({ name, description, price });
+      await newItem.save();
+      res.status(201).json({ message: "Menu item added successfully", data: newItem });
+  } catch (err) {
+      res.status(500).json({ error: err.message });
+  }
 });
 
 app.listen(port, () => {
